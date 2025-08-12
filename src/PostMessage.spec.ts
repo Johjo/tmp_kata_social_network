6d433b36c1e7ccef030b1b38322e32a3b7bbdde3
Romain <romain.code@pm.me>
2025-08-12 14:40:37 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/PostMessage.spec.ts

--- src/PostMessage.spec.ts
+++ src/PostMessage.spec.ts
@@ -1,3 +1,10 @@
+import { describe, expect, it } from 'vitest';
+
 describe('Should post a message', () => {
 
+  it('should post a one line message', () => {
+
+    let messageRepo: MessageRepository = new InMemoryMessageRepository() ;
+    expect(messageRepo).contain("je suis le premier message");
+  });
 });
