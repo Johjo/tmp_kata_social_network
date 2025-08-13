0677cd4714967545de46ad71d9ea26201afabdbc
Jonathan <johjo@ytreza.org>
2025-08-13 16:17:18 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/Acceptance.spec.ts

--- src/Acceptance.spec.ts
+++ src/Acceptance.spec.ts
@@ -1,10 +1,29 @@
 import { describe, expect, it } from 'vitest';
 
 
+class Controller {
+  follow(follower: string, followed: string) {
+
+  }
+
+  seePost(user: string) {
+    return []
+  }
+}
 
 describe('Should handle use cases', () => {
 
   it('should post a one line message', () => {
+    const controller = new Controller()
+
+    controller.follow("Alice", "Bob")
+
+    const posts = controller.seePost("Alice")
+
+    expect(posts).toStrictEqual([]);
+
+
+
 
   });
 
