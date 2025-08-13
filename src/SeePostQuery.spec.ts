622bed812aef2302fb7e718bffea2006765f38d6
Frédéric JAMMES <25798737+fredjammes@users.noreply.github.com>
2025-08-13 16:49:42 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/SeePostQuery.spec.ts

--- src/SeePostQuery.spec.ts
+++ src/SeePostQuery.spec.ts
@@ -1,4 +1,5 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import { MessageRepositoryPort } from './PostMessage.spec';
 
 export class SeePostUseCase {
   query() {
@@ -6,9 +7,26 @@ export class SeePostUseCase {
   }
 }
 
+class InMemoryMessageRepository implements MessageRepositoryPort {
+  messages: string[] = [];
+
+
+  save(message: string): void {
+    this.messages.push(message);
+  }
+
+  all() {
+    return this.messages;
+  }
+}
+
 describe('SeePostQuery', () => {
   it('Should return a list of posts', () => {
+    const messageRepository = new InMemoryMessageRepository();
+    messageRepository.save("coucou");
     const seePostUseCase = new SeePostUseCase();
+
+    expect(seePostUseCase.query()).toStrictEqual(messageRepository.all());
   });
 });
 
